2e982b097277eae3c8906ef6ae5f45e970ba196c
Abdullah Barakat <abdallah771363885@gmail.com>
2024-06-05 21:36:08 +0300

    feat : add sidebar

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,57 @@
+
+import { Menu } from "primereact/menu";
+
+import NavItemTemplate from "./NavItemTemplate";
+
+
+
+const items = [
+    {
+        label: 'Dummy Json',
+        items:[
+            {
+                label: 'Home',
+                 icon: 'i-tabler-home',
+                  template: NavItemTemplate,
+                   page: '/',
+                    
+                },
+                
+            {
+                label: 'Cart',
+                 icon: 'i-tabler-shopping-cart',
+                  template: NavItemTemplate,
+                   page: '/cart',
+                    
+                },
+            {
+                label: 'Checkout',
+                 icon: 'i-tabler-check',
+                  template: NavItemTemplate,
+                   page: '/checkout',
+                    
+                },
+                {
+                    label: 'Add Product',
+                     icon: 'i-tabler-plus',
+                      template: NavItemTemplate,
+                       page: '/product/add',
+                        
+                    },
+        ]
+    }
+    
+
+];
+export default function SideBar() {
+    return (
+        <div className="h-screen w-16rem">
+
+            <div className="card flex justify-center h-full fixed">
+                <Menu pt={{
+                    root: { className: 'rounded-l-0!' },
+                }} model={items} className="w-full md:w-15rem" />
+            </div>
+        </div>
+    )
+}
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -2,16 +2,17 @@
 import { Outlet } from 'react-router-dom'
 import NavBar from '../components/NavBar'
 import AuthGuard from './root/auth/_guard/AuthGuard'
+import SideBar from '../components/SideBar'
 
 export default function Layout() {
 
 
   return (
     <AuthGuard>
-            <>
-      <NavBar/>
-      <Outlet/>
-    </>
+      <div className='w-screen min-h-screen flex'>
+        <SideBar />
+        <Outlet />
+      </div>
     </AuthGuard>
 
   )
